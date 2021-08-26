fc02d15871f0d3e52826f5451eab15bc33aa3815
daviddigital <davidwatts11@gmail.com>
2021-08-26 23:15:34 +1000

    Create datasample.js

---
+++ datasample.js
@@ -0,0 +1,46 @@
+{
+ "Addresses": [
+ {
+ "Line1": "The First ave",
+ "Line2": "",
+ "City": "City X",
+ "State": "NewState",
+ "Postcode": "12345",
+ "Country": "US",
+ "Type": "Shipping",
+ "DefaultForType": true
+ },
+ {
+ "Line1": "The First ave",
+      "Line2": "",
+ "City": "City X",
+ "State": "NewState",
+ "Postcode": "12345",
+ "Country": "US",
+ "Type": "Billing",
+ "DefaultForType": true
+ }
+ ],
+ "Contacts": [
+ {
+ "Name": "John Smith",
+ "Phone": "1-555-555-55-55",
+ "Email": "email@sample.com",
+ "Default": true
+ }
+ ],
+ "Name": "Sample Company",
+ "Currency": "USD",
+ "PaymentTerm": "30 days term",
+ "Discount": 0,
+ "TaxRule": "Tax on Sales",
+ "Carrier": "DHL",
+ "Location": "Main Warehouse",
+ "Comments": "Sample customer",
+ "AccountReceivable": "610",
+ "RevenueAccount": "200",
+ "PriceTier": "Default Price Tier",
+ "Tags": "sample, customer",
+ "Status": "Active",
+ "CreditLimit": 0
+}
